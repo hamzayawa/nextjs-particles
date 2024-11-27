f4e97f5bd8197ce377bafad89779c24834040f3f
Hamza Abdullahi <coachbabaa@gmail.com>
2024-11-27 13:23:42 +0100

    Initial commit

--- login/types/index.ts
+++
@@ -1,9 +0,0 @@
-// types/index.ts
-export interface FormErrors {
-    firstname: string;
-    lastname: string;
-    username: string;
-    email: string;
-    password: string;
-    confirmPassword: string;
-  }
